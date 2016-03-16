93354354265bcb6bc9869a823711360ce8cbdb4d
razat249 <razat249@gmail.com>
2016-03-16 12:33:04 +0530

    Added some stuff login and gallery

--- js/master.js
+++ js/master.js
@@ -28,6 +28,14 @@
 		$scope.societies = societies;
 	}]);
 
+	app.controller('LoginCtrl',['$scope', function($scope){
+		
+	}]);
+
+	app.controller('GalleryCtrl',['$scope', function($scope){
+		$scope.pupu = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/office.jpg"];
+	}]);
+
 
 // routing
 	app.config([
@@ -37,10 +45,22 @@
 
 	    $stateProvider
 	        .state('home', {
-	        url: '/home',
-	      	templateUrl: '/home.html',
-	      	controller: 'HomeCtrl'
-	    })
+		        url: '/home',
+		      	templateUrl: '/home.html',
+		      	controller: 'HomeCtrl'
+	    	})
+
+	    	.state('login', {
+		        url: '/login',
+		      	templateUrl: '/login.html',
+		      	controller: 'LoginCtrl'
+	    	})
+
+	    	.state('gallery', {
+		        url: '/gallery',
+		      	templateUrl: '/gallery.html',
+		      	controller: 'GalleryCtrl'
+	    	})
 
 	  	$urlRouterProvider.otherwise('home');
 	}]);
